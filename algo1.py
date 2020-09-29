6371c9e8ace3da9913eadbf6d0406d28762dde3c
bilal bedine <bilal.bedine@devinci.fr>
2020-09-29 11:59:46 +0200

    commit algo 2

--- algo1.py
+++ algo1.py
@@ -1,6 +1,6 @@
 def algo1():
     tab = []
-    for i in range(1, 1000):
+    for i in range(1, 999):
         convertToString = str(i)
         if i < 100:
             convertToString = convertToString.zfill(3)
@@ -8,6 +8,8 @@ def algo1():
         sortedTab = sorted(array)
         if sortedTab not in tab:
             tab.append(sortedTab)
-            print("".join(list(map(str, sortedTab))))
+            if array[0] != array[1] and array[1] != array[2] and array[2] != array[0] :
+                print("".join(list(map(str, sortedTab))))
+
 
 algo1()
