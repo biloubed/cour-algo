e98960e91aba839b307bab6389a7fae52e4ce2fe
bilal bedine <bilal.bedine@devinci.fr>
2020-09-29 11:40:33 +0200

    commit algo

--- algo1.py
+++ algo1.py
@@ -1,13 +1,13 @@
 def algo1():
-    storage = []
+    tab = []
     for i in range(1, 1000):
         convertToString = str(i)
         if i < 100:
             convertToString = convertToString.zfill(3)
         array = list(map(int, list(convertToString)))
-        sortedArray = sorted(array)
-        if sortedArray not in storage:
-            storage.append(sortedArray)
-            print("".join(list(map(str, sortedArray))))
+        sortedTab = sorted(array)
+        if sortedTab not in tab:
+            tab.append(sortedTab)
+            print("".join(list(map(str, sortedTab))))
 
 algo1()
